c2b278d1d730ed00c0f5ad72e033787779514ef6
Christian Wirtz <doctore74@users.noreply.github.com>
2022-03-24 09:37:04 +0000

    issuing system commands with Go

--- systemcmds01.go
+++ systemcmds01.go
@@ -1,22 +1,48 @@
 /* Alta3 Research | RZFeeser
    Executing system commands  */
 
+// package main
+// 
+// import (
+//     "log"
+//     "os/exec"
+// )
+// 
+// func main() {
+// 
+//     // prepares a "cmd" struct
+//     cmd := exec.Command("ls")
+// 
+//     err := cmd.Run()
+// 
+//     if err != nil {
+//         log.Fatal(err)
+//     }
+// }
+
 package main
 
 import (
     "log"
     "os/exec"
+    "fmt"
+    "bytes"
 )
 
 func main() {
 
     // prepares a "cmd" struct
     cmd := exec.Command("ls")
+    var out bytes.Buffer
+    cmd.Stdout = &out
 
     err := cmd.Run()
 
     if err != nil {
         log.Fatal(err)
     }
+
+    fmt.Print(out.String())
 }
+
 
